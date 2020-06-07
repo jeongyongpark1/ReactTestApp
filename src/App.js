05f232823a9eec0c8ee75d1233092682ac092d0d
jeongyongpark1 <jeongyongpark1@github.com>
2020-06-07 23:30:57 +0900

    Add Router Module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
+import {HashRouter as Router, Route } from 'react-router-dom';
 import AppShell from './AppShell';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Texts from './Texts';
+import Words from './Words';
 
 class App extends Component {
   render() {
     return (
-      <AppShell/>
+      <Router>
+        <AppShell>
+          <div>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/texts" component={Texts}/>
+              <Route exact path="/words" component={Words}/>
+          </div>
+        </AppShell>
+      </Router>
     );
   }
 }
